9cef098feea0adfe8dfeecf3c4b662fcccd4fef6
Madhan-Prasad <gmadhanprasad12@gmail.com>
2024-05-26 01:01:35 +0530

    added imports and its initial conditions

---
+++ Earths orbit around the sun/earth.py
@@ -0,0 +1,26 @@
+#imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+# constants 
+G = 6.6743e-11
+M_sun = 1.989e30
+
+# initial position and vector 
+r_0 = np.array([147.1e9,0])
+v_0 = np.array([0, -30.29e3])
+
+# time step and total time for simulation 
+dt = 3600 
+t_max = 3.154e7
+
+# time array to be used in the numerical solution  
+t = np.arange(0, t_max, dt)
+print(t.astype('int32'))
+
+# initialize arrays to store position and velocities at all the time steps
+r= np.empty(shape=(len(t),2 ))
+v= np.empty(shape=(len(t),2 ))
+
+#set the initial conditions for positions and velocities 
+r[0], v[0] =r_0 , v_0
