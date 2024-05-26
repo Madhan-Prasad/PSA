bdae0c8b9e41e2508ce46b768c46908e38459b14
Madhan-Prasad <gmadhanprasad12@gmail.com>
2024-05-26 20:09:58 +0530

    added stars to pygame window

--- Solar System Simulator/solar_system.py
+++ Solar System Simulator/solar_system.py
@@ -13,8 +13,24 @@ BLACK = (0,0,0)
 screen_info= pg.display.Info()
 WIDTH= screen_info.current_w
 HEIGHT = screen_info.current_h
-WINDOW = pg.display.set_mode((WIDTH-150,HEIGHT-150))
+WINDOW = pg.display.set_mode((WIDTH,HEIGHT))
 pg.display.set_caption('Solar Syatem Simulator ')
+
+# Stars list with color, center and radius information 
+stars_list = [
+    {
+        'color' : (randint(190,255), randint(190,255), randint(190,255)),
+        'center' : (randint(5, WIDTH-5), randint(5, HEIGHT-5)),
+        'radius' : (randint(1,2))
+    }
+    for star in range(450)
+]
+
+print(stars_list)
+#creating a funtion to draw stars on the simulation 
+def draw_star(stars_list):
+    for star in stars_list:
+        pg.draw.circle(WINDOW, star['color'],star['center'], star['radius'])
 
 
 # Create simulation 
@@ -22,9 +38,9 @@ run=True
 while run:
     
     WINDOW.fill(BLACK)
-    
+    draw_star(stars_list)
     for event in pg.event.get():
-        if event.type == pg.QUIT:
+        if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
          run = False    
     
     pg.display.update()     
