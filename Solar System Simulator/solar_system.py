6ce3f9d5f30d3c3e6c50c38d5e9a59aeadc76746
Madhan-Prasad <gmadhanprasad12@gmail.com>
2024-05-26 20:36:38 +0530

    added class for the simulation

--- Solar System Simulator/solar_system.py
+++ Solar System Simulator/solar_system.py
@@ -8,6 +8,11 @@ pg.init()
 
 # Colour 
 BLACK = (0,0,0)
+YELLOW = (225,225,0)
+GREY = (128,128,128)
+YELLOWISH_WHITE = (225,225,246)
+BLUE = (0,0, 225)
+RED = (188,39,50)
 
 # Creat window
 screen_info= pg.display.Info()
@@ -26,14 +31,39 @@ stars_list = [
     for star in range(450)
 ]
 
-print(stars_list)
 #creating a funtion to draw stars on the simulation 
 def draw_star(stars_list):
     for star in stars_list:
         pg.draw.circle(WINDOW, star['color'],star['center'], star['radius'])
 
+#creating a class SolarSystemBodies
+class SolarSystemBodies :
+    
+    AU=1.496e11
+    SCALE = 285/AU
+    
+    #constructor
+    def __init__(self, name, color, x, y, mass, radius ):
+        self.name= name
+        self.color= color
+        self.x= x
+        self.y=y
+        self.mass= mass
+        self.radius= radius
+        
+        # method 1 -draw the bodies on the simulator 
+    def draw_body(self, WINDOW):
+            x= self.x*SolarSystemBodies.SCALE + WIDTH//2
+            y= self.y*SolarSystemBodies.SCALE + HEIGHT//2
+            pg.draw.circle(surface=WINDOW, color= self.color, center=(x,y), radius=self.radius)
 
 # Create simulation 
+
+sun= SolarSystemBodies("Sun", YELLOW, 0, 0, 1.989e30, 30 )
+mercury= SolarSystemBodies("Mercury", GREY, 0.39*SolarSystemBodies.AU, 0, 0.33e24, 6)
+venus= SolarSystemBodies("venus", YELLOWISH_WHITE, 0.72*SolarSystemBodies.AU, 0, 4.87e24, 14)
+earth= SolarSystemBodies("earth", BLUE, 1*SolarSystemBodies.AU, 0, 5.97e24, 15)
+mars= SolarSystemBodies("Mars", RED, 1.52*SolarSystemBodies.AU, 0, 0.642e24, 8)
 run=True
 while run:
     
@@ -42,7 +72,9 @@ while run:
     for event in pg.event.get():
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
          run = False    
-    
+    ss_bodies=[sun,mercury, venus, earth, mars]
+    for body in ss_bodies:
+        body.draw_body(WINDOW)
     pg.display.update()     
          
 pg.quit()         
