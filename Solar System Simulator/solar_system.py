7aa177d055cf67adeb8534ca7cc8036ff8387c49
Madhan-Prasad <gmadhanprasad12@gmail.com>
2024-05-26 19:49:50 +0530

    added pygame window for the simulation

--- Solar System Simulator/solar_system.py
+++ Solar System Simulator/solar_system.py
@@ -1,4 +1,25 @@
 # Libraries
-import pygame as py
+import pygame as pg
 import math 
 from random import randint 
+
+# Initialize Pygame
+pg.init()
+
+# Creat window
+screen_info= pg.display.Info()
+WIDTH= screen_info.current_w
+HEIGHT = screen_info.current_h
+WINDOW = pg.display.set_mode((WIDTH-150,HEIGHT-150))
+
+
+# Create simulation 
+run=True
+while run:
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+         run = False    
+         
+pg.quit()         
+         
