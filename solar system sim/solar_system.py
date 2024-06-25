0fdc69d4929c71a2ff5e3eccc89dd3d6dbea3c0b
Madhan-Prasad <gmadhanprasad12@gmail.com>
2024-06-25 23:59:05 +0530

    added class containing the planets and informations

--- solar system sim/solar_system.py
+++ solar system sim/solar_system.py
@@ -16,6 +16,33 @@ pg.display.set_caption('Solar System Simulation')
 
 #Colours
 BLACK=(0,0,0)
+YELLOW=(225,225,0)
+GRAY = (128,128,128)
+YELLOWISH_WHITE=(225,225,246)
+BLUE= (0,0,225)
+RED= (188,39,50)
+# class solar system bodies
+class SolarSystemBodies :
+    
+    AU = 1.496e11
+    SCALE = 280/AU
+    
+    #constructor
+    def __init__(self, name, color, x, y, mass, radius):
+        self.name = name
+        self.color = color
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.radius = radius
+        
+    #method 01- Draw the bodies on the simulator 
+    def draw_body(self, WINDOW):
+        x = self.x*SolarSystemBodies.SCALE + WIDTH//2 
+        y = self.y*SolarSystemBodies.SCALE + HEIGHT//2
+        pg.draw.circle(surface=WINDOW, color= self.color, center= (x,y), radius=self.radius)
+           
+    
 
 # Star list with colour, center and radius information 
 stars_list = [
@@ -35,6 +62,12 @@ def draw_stars(stars_list):
 
 #Create Simulation 
 run = True
+
+sun = SolarSystemBodies("SUN", YELLOW, 0,0, 1.9891e30, 30)
+mercury = SolarSystemBodies("MERCURY", GRAY,0.39*SolarSystemBodies.AU, 0, 0.33e24, 6)
+venus= SolarSystemBodies("VENUS",YELLOWISH_WHITE, 0.72*SolarSystemBodies.AU,0, 4.87e24,14)
+earth= SolarSystemBodies("EARTH",BLUE, 1*SolarSystemBodies.AU,0, 5.97e24,15)
+mars= SolarSystemBodies("MARS",RED, 1.52*SolarSystemBodies.AU,0, 0.642e24,8)
 while run:
     
     WINDOW.fill(BLACK)
@@ -42,7 +75,9 @@ while run:
     for event in pg.event.get():
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             run = False        
-            
+    ss_bodies= [sun,mercury,venus,earth,mars]
+    for body in ss_bodies:
+        body.draw_body(WINDOW)        
     pg.display.update()          
 #Quit the pygame
 pg.quit()            
