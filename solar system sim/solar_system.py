7a64ffd0a0cd27b0440409683806a940309e0384
Madhan-Prasad <gmadhanprasad12@gmail.com>
2024-06-26 01:09:53 +0530

    added the tracking of the orbits of the planets

--- solar system sim/solar_system.py
+++ solar system sim/solar_system.py
@@ -21,6 +21,13 @@ GRAY = (128,128,128)
 YELLOWISH_WHITE=(225,225,246)
 BLUE= (0,0,225)
 RED= (188,39,50)
+NAME_TEXT_COLOR = (111,236,123)
+DIST_TEXT_COLOR = (56,190,255)
+SUN_NAME_COLOR = (144,128,254)
+SUN_TEXT_COLOR = (54,32,12)
+# set up fonts
+NAME_TEXT = pg.font.SysFont(name='TimesRoman', size= 18, bold= True)
+DIST_TEXT = pg.font.SysFont(name='Sans', size= 18, bold= True)
 # class solar system bodies
 class SolarSystemBodies :
     
@@ -40,12 +47,25 @@ class SolarSystemBodies :
         self.x_vel = 0
         self.y_vel = 0
         self.orbit = []
-        
+        self.sun = False
+        self.distance_to_sun = 0
     #method 01- Draw the bodies on the simulator 
     def draw_body(self, WINDOW):
         x = self.x*SolarSystemBodies.SCALE + WIDTH//2 
         y = self.y*SolarSystemBodies.SCALE + HEIGHT//2
         pg.draw.circle(surface=WINDOW, color= self.color, center= (x,y), radius=self.radius)
+        
+        if not self.sun:
+            name_text = NAME_TEXT.render(self.name , True, NAME_TEXT_COLOR)
+            WINDOW.blit(name_text,(x-40 , y-55))
+            dist_text = DIST_TEXT.render(f"{round(self.distance_to_sun/(3e8*60),3)}lt-min" , True, NAME_TEXT_COLOR)
+            WINDOW.blit(dist_text,(x-40 , y-35))
+        else:    
+            name_text = NAME_TEXT.render(self.name , True, SUN_NAME_COLOR)
+            WINDOW.blit(name_text,(x-40 , y-65))
+            dist_text = DIST_TEXT.render(f"{round(self.x / 3e8,3),round(self.y / 3e8,3)}lt-sec" , True, NAME_TEXT_COLOR)
+            WINDOW.blit(dist_text,(x-40 , y-55))
+            
            
     # Method 02- TO alculate the gravitational force between the planets 
     def gravitational_force(self, ss_body):
@@ -53,6 +73,8 @@ class SolarSystemBodies :
         x_diff = ss_body.x - self.x
         y_diff = ss_body.y - self.y
         distance = math.sqrt(x_diff**2 + y_diff**2)
+        if ss_body.sun:
+            self.distance_to_sun = distance
         g_force= self.G * self.mass * ss_body.mass / distance**2
         theta = math.atan2(y_diff,x_diff)
         f_x = g_force * math.cos(theta)
@@ -73,6 +95,22 @@ class SolarSystemBodies :
         self.y += self.y_vel * self.TIME_STEP
         self.orbit.append((self.x, self.y))        
      
+    # Method 04 - To track the orbit of the Solar System Bodies 
+    def track_orbit(self, WINDOW):
+         if len(self.orbit) > 1:
+             centered_points = []
+             for (x,y) in self.orbit:
+                 x = x*self.SCALE + WIDTH//2
+                 y = y*self.SCALE + HEIGHT//2
+                 centered_points.append((x,y))
+             pg.draw.lines(surface=WINDOW, color= self.color, closed=False, points= centered_points, width= 2)
+             
+    # Method 05 - Draw
+    def draw(self, WINDOW, track = True):
+        self.draw_body(WINDOW)
+        if track:
+            self.track_orbit(WINDOW)
+                    
 
 # Star list with colour, center and radius information 
 stars_list = [
@@ -95,6 +133,7 @@ run = True
 paused = False
 
 sun = SolarSystemBodies("SUN", YELLOW, 0,0, 1.9891e30, 30)
+sun.sun = True
 mercury = SolarSystemBodies("MERCURY", GRAY,0.39*SolarSystemBodies.AU, 0, 0.33e24, 6)
 mercury.y_vel= -47.4e3
 venus= SolarSystemBodies("VENUS",YELLOWISH_WHITE, 0.72*SolarSystemBodies.AU,0, 4.87e24,14)
@@ -124,7 +163,7 @@ while run:
         ss_bodies= [sun,mercury,venus,earth,mars]
         for body in ss_bodies:
          body.update_position(ss_bodies)
-         body.draw_body(WINDOW)        
+         body.draw(WINDOW , track= True)        
         pg.display.update()          
 #Quit the pygame
 pg.quit()            
